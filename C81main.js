16888f2adaae3204199b682a5e2b10c4b6dd9247
SHAN1030 <34219879+SHAN1030@users.noreply.github.com>
2022-07-04 11:53:30 -0600

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,40 @@
+canvas = document.getElementById("myCanvas");
+
+ctx = canvas.getContext("2d");
+
+ctx.beginPath();        
+ctx.strokeStyle = "gray"; 
+ctx.lineWidth = 1;      
+ctx.rect(150, 143, 430,200);   
+ctx.stroke();
+
+ctx.beginPath();        
+ctx.strokeStyle = "blue"; 
+ctx.lineWidth = 5;      
+ctx.arc(270, 225, 40,0,2*Math.PI);   
+ctx.stroke();
+
+ctx.beginPath();        
+ctx.strokeStyle = "black"; 
+ctx.lineWidth = 5;      
+ctx.arc(450, 225, 40,0,2*Math.PI);   
+ctx.stroke();
+
+ctx.beginPath();        
+ctx.strokeStyle = "red"; 
+ctx.lineWidth = 5;      
+ctx.arc(360, 225, 40,0,2*Math.PI);   
+ctx.stroke();
+
+
+ctx.beginPath();        
+ctx.strokeStyle = "yellow"; 
+ctx.lineWidth = 5;      
+ctx.arc(310, 275, 40,0,2*Math.PI);   
+ctx.stroke();
+
+ctx.beginPath();        
+ctx.strokeStyle = "green";
+ctx.lineWidth = 5;      
+ctx.arc(410, 275, 40,0,2*Math.PI);   
+ctx.stroke();
